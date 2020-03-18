8334fd56c3d42a30c09a9dfaafae4b4cb155c254
emex4gman <emex4gman@gmail.com>
2020-03-18 19:16:44 +0100

    added account number

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -29,6 +29,10 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now
   },
+  account_number: {
+    type: Number,
+    required: true
+  },
 });
 
 module.exports = mongoose.model('Users', UserSchema);
