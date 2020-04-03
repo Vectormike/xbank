0d40efb7781df32aa2e7155412425c879b96daed
Redeemer40 <victorjonah199@gmail.com>
2020-04-03 17:12:07 +0100

    ...

--- server/app.js
+++ server/app.js
@@ -6,15 +6,17 @@ const app = express();
 
 connectDB();
 
-// Init Middleware
+/**
+ * Init Middleware
+ */
 
 app.use(morgan("dev"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /**
  * Primary routes
  */
+app.get("/", (req, res) => res.send("Welcome to xbank"));
 
 module.exports = app;
